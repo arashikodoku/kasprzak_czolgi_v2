1ded45bb11eb5d3affdf16e22f323bf27542ca85
Konrad Napiorkowski <k.napiorkows@partner.samsung.com>
2017-10-06 13:41:21 +0200

    Mapa + muzyka

--- core/src/com/samsung/czolgi/GraCzolgi.java
+++ core/src/com/samsung/czolgi/GraCzolgi.java
@@ -2,27 +2,54 @@ package com.samsung.czolgi;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class GraCzolgi extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Texture img;
+	private Music music;
+	private OrthographicCamera camera;
+	private ShapeRenderer shapeRenderer;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+		music = Gdx.audio.newMusic(Gdx.files.internal("XX.mp3"));
+		music.setLooping(true);
+		music.play();
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		batch = new SpriteBatch();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.5f, 0.8f, 0.9f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+//		batch.draw(img, 0, 0);
 		batch.end();
+
+		shapeRenderer = new ShapeRenderer();
+		shapeRenderer.setProjectionMatrix(camera.combined);
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.setColor(Color.BROWN);
+		shapeRenderer.rect(0, 0, 800, 200);
+		shapeRenderer.end();
 	}
 	
 	@Override
